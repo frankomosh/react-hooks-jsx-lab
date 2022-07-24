7d893783fbd610725471b7fb2905811a5d8282cf
Frank-197 <frankomosh197@gmail.com>
2022-07-24 19:31:35 +0300

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,9 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
   return <div id="home">
-    <h1 style={{color : 'firebrick'}}>{name} is a web developer from {city} </h1>
-    
-    </div>;
+    <h1 style={{color :"firebrick"}}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
